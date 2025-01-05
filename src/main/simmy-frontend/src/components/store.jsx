7d43d80e60dd6ffe91b23e541f4a142178cb3e66
Vaibhav Malhotra <vaibhav@Vaibhavs-MacBook-Air-4.local>
2025-01-04 17:06:59 -0800

    Created global redux variable in frontend:

--- src/main/simmy-frontend/src/components/store.jsx
+++ src/main/simmy-frontend/src/components/store.jsx
@@ -5,6 +5,33 @@ import storage from 'redux-persist/lib/storage'; // defaults to localStorage for
 // Initial state
 const initialState = {
   user: null, // This will store your login data globally
+  campaign: {
+    Id: '-1', // Campaign ID
+    Objective: '', // Campaign objective
+    AdSetIds: [], // Array of AdSet IDs
+  },
+  adset: {
+    Id: '',
+    Name: '',
+    Type: '',
+    Pixel: '',
+    ConversionEvent: '',
+    BudgetType: '',
+    BudgetAmount: '',
+    AdIds: [],
+  },
+  ad: {
+    Id: '',
+    Name: '',
+    AdSetup: '',
+    Format: '',
+    Text: '',
+    Headline: '',
+    Description: '',
+    CallToAction: '',
+    Image: '',
+    Video: '',
+  },
 };
 
 // Reducer function to update the state
@@ -30,6 +57,30 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         user: action.payload, // Directly set the user data from the backend
+      };
+      case 'SET_CAMPAIGN':
+      return {
+        ...state,
+        campaign: {
+          ...state.campaign, // Keep existing campaign fields
+          ...action.payload, // Update campaign fields
+        },
+      };
+      case 'SET_ADSET':
+      return {
+        ...state,
+        adset: {
+          ...state.adset, // Keep existing adset fields
+          ...action.payload, // Update adset fields
+        },
+      };
+    case 'SET_AD':
+      return {
+        ...state,
+        ad: {
+          ...state.ad, // Keep existing ad fields
+          ...action.payload, // Update ad fields
+        },
       };
     default:
       return state;
